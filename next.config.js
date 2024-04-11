3598a55cc18d235fed9d50621869e9eee8dfbbb6
Brandon Garcia <44207909+BUGR2487@users.noreply.github.com>
2024-04-11 13:41:55 -0600

    Se agrego cloudinary al next config

--- next.config.js
+++ next.config.js
@@ -18,6 +18,10 @@ const nextConfig = {
         protocol: "https",
         hostname: "raw.githubusercontent.com",
       },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
     ],
   },
 };
